4ebd3151f5fe06887234f7a5815ca246ebb78c1e
RC_Student_lab <RC_Student_lab@labG9AEB3.54cfxevd0dhuxlzdtb111i3pfc.fx.internal.cloudapp.net>
2024-08-08 11:58:58 +0000

    First Commit

---
+++ src/github/link/GitHubLink.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github.link;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class GitHubLink {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.printin("Hello World")
+    }
+    
+}
